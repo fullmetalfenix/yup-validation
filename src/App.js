94e92a679b3797118aacc414c4ea4687f7bfb898
Mr <jpihlk@yahoo.com>
2022-12-05 17:08:32 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+export default function BasicTextFields() {
+  
+  const save = (event) => {
+  event.preventDefault();
+  alert('hello world')
 }
 
-export default App;
+  return (
+    <Box
+      component="div"
+      style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center' , alignItems: 'center'}}
+      noValidate
+      autoComplete="off"
+    >
+      <form
+      //method="POST"
+      onSubmit={(event) => {save(event)}}
+      style={{
+        width: 300,
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        flexDirection: 'column',
+        margin: '0px auto',
+        height: 400,
+      }}
+      id="basic-form"
+      >
+        <TextField required id="standard-basic" label="Name" variant="standard" />
+        <TextField required  id="standard-basic" label="Email" type="email" variant="standard" />
+        <TextField required id="standard-basic" label="Phone Number" type="tel" variant="standard" />
+        <TextField required id="standard-basic" label="Favorite Number" type="number" variant="standard" />
+        <Button type="submit" variant="extended" id="save-button" color="promary"
+        style={{
+          "padding": 10,
+          "background": "navy",
+          color: 'white',
+          width: 160,
+          borderRadius: 40
+        }}
+        
+        >Save</Button>
+      </form>
+    </Box>
+  );
+}
