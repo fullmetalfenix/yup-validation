7b82354b840231703cd72335bc917322389487c7
Mr <jpihlk@yahoo.com>
2022-12-08 19:36:36 -0500

    added optional but has to be _ if included logic for zip

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ import { object } from "yup/lib/locale";
 
 let schema = yup.object().shape({
   name: yup.string().required("Name is a Required Field"),
-  // age: yup.number().required().positive().integer(),
   email: yup.string().required("Email is a Required Field").email(), // dont need .email error message as mui handling it
-  //  number: yup.number().max(20) // highest length
+  zip: yup
+    .string()
+    .notRequired()
+    .min(5, "Zip can only be 5 characters.")
+    .max(5, "Zip can only be 5 characters.")
+    .nullable()
+    .transform((value) => (!!value ? value : null)),
   number: yup
     .string()
     .required("Favorite Number is a Required Field")
@@ -23,14 +28,12 @@ let schema = yup.object().shape({
 });
 
 export default function BasicTextFields() {
-
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   // const telRef = useRef(null); // dont need, not validating - would need if actually submitting a form though
   const numberRef = useRef(null);
-
+  const zipRef = useRef(null);
   const [validationResults, setValidationResults] = useState([]);
-
 
   // Form Submit Handler
   const save = (event) => {
@@ -42,28 +45,25 @@ export default function BasicTextFields() {
             name: nameRef.current.value,
             email: emailRef.current.value,
             number: numberRef.current.value,
+            zip: zipRef.current.value,
           },
           { abortEarly: false, context: object } // returns context object of just what was in Schema. ex: no phone in this case
-        ).then(validInfo => {
-           setValidationResults([])
-           alert(`Thanks ${validInfo.name}! Your form has been submitted.`)
+        )
+        .then((validInfo) => {
+          setValidationResults([]);
+          alert(`Thanks ${validInfo.name}! Your form has been submitted.`);
         })
         .catch((err) => {
           var messageArray = [];
           err.inner.forEach((e) => {
-            messageArray.push(e.message) 
+            messageArray.push(e.message);
             //console.log(e.message, e.path);
           });
-          setValidationResults(messageArray)
-
+          setValidationResults(messageArray);
         });
     };
-    validateNestedSchema()
+    validateNestedSchema();
   };
-
-
-
-
 
   return (
     <Box
@@ -102,7 +102,6 @@ export default function BasicTextFields() {
           id="name"
           label="Name"
           variant="standard"
-
         />
         <TextField
           inputRef={emailRef} // NOT ref - inputRef in MUI
@@ -117,6 +116,14 @@ export default function BasicTextFields() {
           type="tel"
           variant="standard"
         />
+        <TextField
+          inputRef={zipRef} // NOT ref - inputRef in MUI
+          id="zip"
+          label="Zip Code"
+          type="number"
+          variant="standard"
+        />
+
         <TextField
           inputRef={numberRef} // NOT ref - inputRef in MUI
           id="number"
@@ -141,10 +148,11 @@ export default function BasicTextFields() {
           Save
         </Button>
 
-        {validationResults.map((validationMessage)=>{
-          return <p style={{margin: 3, color: "firebrick"}}>{validationMessage}</p>
-        }
-        )}
+        {validationResults.map((validationMessage) => {
+          return (
+            <p style={{ margin: 3, color: "firebrick" }}>{validationMessage}</p>
+          );
+        })}
       </form>
     </Box>
   );
